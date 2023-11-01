2cccab04c5d0b353741c293185c3b8f84fc2860e
JhowReis <jonathan.linkedin2019@gmail.com>
2023-11-01 11:13:03 -0300

    Enum OrderStatus

--- src/main/java/com/jonathanreis/curso/entities/Order.java
+++ src/main/java/com/jonathanreis/curso/entities/Order.java
@@ -1,6 +1,7 @@
 package com.jonathanreis.curso.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.jonathanreis.curso.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -25,14 +26,16 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
 
     public Order(){
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -49,6 +52,17 @@ public class Order implements Serializable {
     }
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+
+        }
     }
 
     public User getClient() {
