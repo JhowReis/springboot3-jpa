39cbff0053f8a607bdce5bb2f257484a93da12ad
JhowReis <jonathan.linkedin2019@gmail.com>
2023-10-31 21:47:56 -0300

    JPA repository, injeção de dependência, database seeding

---
+++ src/main/java/com/jonathanreis/curso/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.jonathanreis.curso.repositories;
+
+import com.jonathanreis.curso.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository  extends JpaRepository<User, Long> {
+}
