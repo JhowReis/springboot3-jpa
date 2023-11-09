1bfd19a03f83e53680c90cc1f1aa351bdae07048
JhowReis <jonathan.linkedin2019@gmail.com>
2023-11-09 11:15:51 -0300

    Entidade Payment, associação um para um

--- src/main/java/com/jonathanreis/curso/entities/Order.java
+++ src/main/java/com/jonathanreis/curso/entities/Order.java
@@ -33,6 +33,8 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order(){
 
@@ -81,6 +83,14 @@ public class Order implements Serializable {
 
     public void setItems(Set<OrderItem> items) {
         this.items = items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
